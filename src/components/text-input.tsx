9fe711e4555405cf98975ba51c9b0cbf979c17e8
forge <28455546+forge34@users.noreply.github.com>
2024-08-01 19:54:15 +0300

    feat : add input validation

--- src/components/text-input.tsx
+++ src/components/text-input.tsx
@@ -1,27 +1,46 @@
 import React, { useState } from "react";
 import "../styles/css/text-input.css";
+import { Link } from "react-router-dom";
+
+interface Password {
+  isPassword: boolean;
+  type: "password" | "new-password";
+}
 
 interface TextInputProps {
   name: string;
   label: string;
-  password?: boolean;
+  error?: {
+    errorMsg: string | undefined;
+  };
+  password?: Password;
 }
 
 export default function TextInput(props: TextInputProps) {
   const [value, setValue] = useState("");
 
+  const state = props.error?.errorMsg ? "invalid" : "";
   return (
-    <div className="text-input">
-      <label>{props.label}</label>
+    <div className={"text-input"}>
+      <div>
+        <label>{props.label}</label>
+        {props.password?.type === "password" && (
+          <Link to="/forgot-password">Forgot your password?</Link>
+        )}
+      </div>
       <input
-        autoComplete="new-password"
-        type={props.password ? "password" : "text"}
+        className={state}
+        autoComplete={props.password?.type}
+        type={props.password?.isPassword ? "password" : "text"}
         value={value}
         name={props.name}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
           setValue(e.target.value);
         }}
       />
+      {props.error?.errorMsg && (
+        <span className="error">{props.error.errorMsg}</span>
+      )}
     </div>
   );
 }
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,22 +1,65 @@
 import { Link } from "react-router-dom";
 import TextInput from "../components/text-input";
 import "../styles/css/form.css";
+import { z } from "zod";
+import { useZorm, Zorm } from "react-zorm";
+
+const FormSchema = z
+  .object({
+    username: z.string().min(1, { message: "Username can not be empty" }),
+    password: z
+      .string()
+      .min(5, { message: "Password should be at least 5 characters" }),
+    confirmpassword: z.string().min(5, "Passwords do not match"),
+  })
+  .refine((values) => {
+    return values.password === values.confirmpassword;
+  }, "Passwords do not match!");
 
 function Signup() {
+  const zodForm: Zorm<typeof FormSchema> = useZorm("signup", FormSchema, {
+    onValidSubmit(e) {
+      e.preventDefault();
+    },
+  });
+
   return (
-    <form className="form">
+    <form className="form" ref={zodForm.ref}>
       <h1>Create account</h1>
-      <TextInput name="username" label="Username"></TextInput>
-      <TextInput name="password" label="Password" password={true}></TextInput>
       <TextInput
-        name="confirmpassword"
+        name={zodForm.fields.username()}
+        error={{
+          errorMsg: zodForm.errors.username()?.message,
+        }}
+        label="Username"
+      ></TextInput>
+      <TextInput
+        name={zodForm.fields.password()}
+        label="Password"
+        password={{
+          isPassword: true,
+          type: "new-password",
+        }}
+        error={{
+          errorMsg: zodForm.errors.password()?.message,
+        }}
+      ></TextInput>
+      <TextInput
+        name={zodForm.fields.confirmpassword()}
         label="Confirm Password"
-        password={true}
+        password={{
+          isPassword: true,
+          type: "new-password",
+        }}
+        error={{
+          errorMsg:
+            zodForm.errors()?.message ||
+            zodForm.errors.confirmpassword()?.message,
+        }}
       ></TextInput>
       <button type="submit" className="btn-submit">
         Create account
       </button>
-
       <p>
         Already have an account? <Link to={"/signin"}>Sign-in</Link>
       </p>
