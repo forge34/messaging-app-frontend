d016c1aae197141badaf32bd9d35d0089569dd41
forge34 <28455546+forge34@users.noreply.github.com>
2024-08-02 16:24:46 +0300

    test : update text-input test

--- src/tests/text-input.test.tsx
+++ src/tests/text-input.test.tsx
@@ -1,27 +1,34 @@
 import { render, screen } from "@testing-library/react";
 import TextInput from "../components/text-input";
+import userEvent from "@testing-library/user-event";
 
 describe("text input test suite", () => {
   test("should match snapshot", () => {
     const { container } = render(
-      <TextInput name="username" label="Username" password={false}></TextInput>,
+      <TextInput name="username" label="Username"></TextInput>,
     );
 
     expect(container).toMatchSnapshot();
   });
-  it("renders label correctly", () => {
-    render(
-      <TextInput name="username" label="Username" password={false}></TextInput>,
-    );
+  test("should render label correctly", () => {
+    render(<TextInput name="username" label="Username"></TextInput>);
 
-    expect(screen.getByText("Username")).toBeInTheDocument();
+    expect(screen.getByText("Username".toUpperCase())).toBeInTheDocument();
   });
 
   test("should render input", () => {
-    render(
-      <TextInput name="username" label="Username" password={false}></TextInput>,
-    );
+    render(<TextInput name="username" label="Username"></TextInput>);
 
     expect(screen.getByRole("textbox")).toBeInTheDocument();
+  });
+
+  test("should change input value on typing", async () => {
+    const user = userEvent.setup();
+    render(<TextInput name="username" label="Username"></TextInput>);
+
+    const input = screen.getByRole<HTMLInputElement>("textbox");
+    await user.type(input, "Forge");
+
+    expect(input.value).toMatch("Forge");
   });
 });
