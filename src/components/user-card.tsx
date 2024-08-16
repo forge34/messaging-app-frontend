9df415776cd303188dfbc9e4a4921d3e4a9ba9a3
forge34 <28455546+forge34@users.noreply.github.com>
2024-08-16 15:28:09 +0300

    feat : create user card component

---
+++ src/components/user-card.tsx
@@ -0,0 +1,19 @@
+import "../styles/css/user-card.css"
+
+interface UserCardProps {
+  imgSrc: string;
+  username: string;
+  isRelated: boolean;
+}
+
+export default function UserCard(props: UserCardProps) {
+  return (
+    <div className="user-card">
+      <img src={`https://robohash.org/${props.imgSrc}`} width={64} height={64} />
+      <h1 className="username">{props.username}</h1>
+      <button className="user-card-btn">
+        {props.isRelated ? "open conversation" : "start conversation"}
+      </button>
+    </div>
+  );
+}
